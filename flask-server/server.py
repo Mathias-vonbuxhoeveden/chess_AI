4f92382c869edca19dfb7599244e19cbfe5b9e21
Mathias von Buxhoeveden <m.von-buxhoeveden@klarna.com>
2022-05-21 22:29:46 +0200

    All of teh codes

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,7 +5,7 @@ import chess
 import random
 
 
-app=Flask(__name__)
+app=Flask(__name__,static_folder='client/build',static_url_path='')
 
 @app.route("/members", methods = ['POST'])
 def members():
